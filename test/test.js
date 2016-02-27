792eb271ddad8f8bd48e880942d4253ab49c5b71
Jim Pick <jim@backblend.com>
2016-02-26 17:02:05 -0800

    Add local/remote test suites
    Not sure if this will work for organization.

--- test/test.js
+++ test/test.js
@@ -1,18 +1,33 @@
 import assert from 'assert'
 import 'babel-polyfill'
 
-describe('Array', () => {
-
-  describe('#indexOf()', () => {
-
-    it('should return -1 when the value is not present', () => {
-
-      assert.equal(-1, [1,2,3].indexOf(5))
-      assert.equal(-1, [1,2,3].indexOf(0))
-
+function local () {
+  describe('local', function () {
+    describe('Array local', function () {
+      describe('#indexOf()', function () {
+        it('should return -1 when the value is not present', function () {
+          assert.equal(-1, [1,2,3].indexOf(5))
+          assert.equal(-1, [1,2,3].indexOf(0))
+        })
+      })
     })
-
   })
+}
 
-})
+function remote () {
+  describe('remote', function () {
+    describe('Array remote', function () {
+      describe('#indexOf()', function () {
+        it('should return -1 when the value is not present', function () {
+          assert.equal(-1, [1,2,3].indexOf(5))
+          assert.equal(-1, [1,2,3].indexOf(0))
+        })
+      })
+    })
+  })
+}
+
+local()
+remote()
+
 
