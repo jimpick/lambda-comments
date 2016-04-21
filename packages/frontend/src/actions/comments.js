b167bef0b3995e5bcabfd6a5700e18272dd5ea30
Jim Pick <jim@backblend.com>
2016-04-20 22:56:49 -0700

    Add required OPTIONS method for CORS
    Use mock response to return needed headers.

--- packages/frontend/src/actions/comments.js
+++ packages/frontend/src/actions/comments.js
@@ -88,6 +88,10 @@ export function postComment ({
         apiPostUrl,
         {
           method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+          },
           body: JSON.stringify(payload),
         }
       )
